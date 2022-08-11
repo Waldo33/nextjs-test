335383d9b3ca0b550c5f52935e36915bf1d8a7eb
Waldo33 <evgenijtser@gmail.com>
2022-08-11 16:33:40 +0300

    Initial commit

---
+++ components/MainContainer.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import A from "../components/A"
+
+const MainContainer = ({children, keywords}) => {
+    return ( 
+        <>
+            <Head>
+                <meta keywords={"waldo nextjs " + keywords}/>
+                <title>Главная</title>
+            </Head>
+           <div className="navbar">
+                <A href={'/'} text={'Главная'}/>
+                <A href={'/users'} text={'Пользователи'}/>
+            </div>
+            <div>
+                {children}
+            </div>
+            <style jsx>
+                {`
+                    .navbar {
+                        background: orange;
+                        padding: 15px;
+                    }
+                `}
+            </style>
+        </>
+     );
+}
+ 
+export default MainContainer;
